a8f087c26123dff66a4e4b38a15165901f263f7f
mel <mchuam@gmail.com>
2018-04-16 23:40:52 +0000

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,23 @@ var request = require('request');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': '49261953c4159767bbd2f52661b60b6422961746'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err);
-  conosle.log("Results: ", result);
+  console.log("Results: ", result);
 });
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader! 👾');
