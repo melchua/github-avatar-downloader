a860148fbc4a4bb1fa8e27f3a399a20b1000a2ff
mel <mchuam@gmail.com>
2018-04-17 03:03:56 +0000

    Fixed token call

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,27 @@
 require('dotenv').config();
+
+
 var request = require('request');
 var fs = require('fs');
 var rowner = process.argv[2];
 var rname = process.argv[3];
 
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function doesEnvExist() {
+  var file = ".env";
+    return (!fs.existsSync(file));
+}
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  if (doesEnvExist() === true) {
+    console.log("Be careful, .env does not exist.");
+    return;
+  }
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token' + process.env.GITHUB_TOKEN
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN
     }
   };
 
@@ -40,24 +50,36 @@ function downloadImageByURL(url, filePath) {
 
 
 // introduce error checking to require at the 2 arguments
-if (rowner === undefined || rname === undefined) {
+if (process.argv.length !== 4) {
   console.log("This application requires 2 arguments: Repo Owner and Repo Name. Please enter and try again.");
-} else {
+  return;
+}
 
-  console.log('Welcome to the GitHub Avatar Downloader! 👾');
+console.log('Welcome to the GitHub Avatar Downloader! 👾');
 
 // when refactoring we could name the anonymous call back function something like "get avatar urls"
-  getRepoContributors(rowner, rname, function(err, result) {
-    var data = JSON.parse(result);
+getRepoContributors(rowner, rname, function(err, result) {
 
-    var dir = "./avatars";
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
-    }
+  if (err) {
+    console.log("Something be broke: ");
+    console.error(err);
+    return;
+  }
+  var data = JSON.parse(result);
 
-    data.forEach(function (contrib) {
-      downloadImageByURL(contrib.avatar_url, 'avatars/' + contrib.login + '.jpg');
-    });
+  if (data.message === "Not Found") {
+    console.log("Repo or Repo Owner doesn't exist");
+    return;
+  }
+
+  var dir = "./avatars";
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+
+  data.forEach(function (contrib) {
+    downloadImageByURL(contrib.avatar_url, 'avatars/' + contrib.login + '.jpg');
   });
+});
 
-}
+
